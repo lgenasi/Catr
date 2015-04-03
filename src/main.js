1d30398f051f50684d83c0bf400cb25238f557d1
lgenasi <louis.genasi@gmail.com>
2015-04-03 19:00:01 +0100

    Stopped the carousel from auto-scrolling

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 $(window).ready(function() {
   $('#abuseDialog').modal('show');
+  $('.carousel').carousel({
+    interval: false
+  }) 
   var allowAbuseBtn = document.getElementById("allowAbuseBtn");
   allowAbuseBtn.onclick = function(){
     $('#abuseDialog').modal('hide');
     var webSocket = webSocketHandler.createWebSocket();
-  	
-  
+    
+    
     var sendingId = setInterval(function () {
     	if(webSocket.readyState == 1) {
     		console.log("sending initial packet");
